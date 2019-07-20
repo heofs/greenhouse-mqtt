c63716927df64229b6b76a9134836e314d48cebb
Henning Ofstad <ofstad10@gmail.com>
2019-07-20 18:15:15 +0100

    Move docs

--- publisher/publisher.py
+++ publisher/publisher.py
@@ -1,46 +1,3 @@
-# Copyright 2017 Google Inc. All rights reserved.
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-r"""Sample device that consumes configuration from Google Cloud IoT.
-This example represents a simple device with a temperature sensor and a fan
-(simulated with software). When the device's fan is turned on, its temperature
-decreases by one degree per second, and when the device's fan is turned off,
-its temperature increases by one degree per second.
-
-Every second, the device publishes its temperature reading to Google Cloud IoT
-Core. The server meanwhile receives these temperature readings, and decides
-whether to re-configure the device to turn its fan on or off. The server will
-instruct the device to turn the fan on when the device's temperature exceeds 10
-degrees, and to turn it off when the device's temperature is less than 0
-degrees. In a real system, one could use the cloud to compute the optimal
-thresholds for turning on and off the fan, but for illustrative purposes we use
-a simple threshold model.
-
-To connect the device you must have downloaded Google's CA root certificates,
-and a copy of your private key file. See cloud.google.com/iot for instructions
-on how to do this. Run this script with the corresponding algorithm flag.
-
-  $ python cloudiot_pubsub_example_mqtt_device.py \
-      --project_id=my-project-id \
-      --registry_id=example-my-registry-id \
-      --device_id=my-device-id \
-      --private_key_file=rsa_private.pem \
-      --algorithm=RS256
-
-With a single server, you can run multiple instances of the device with
-different device ids, and the server will distinguish them. Try creating a few
-devices and running them all at the same time.
-"""
-
 import datetime
 import json
 import os
@@ -86,7 +43,7 @@ def error_str(rc):
     return '{}: {}'.format(rc, mqtt.error_string(rc))
 
 
-class Device(object):
+class Publisher(object):
     """Represents the state of a single device."""
 
     def __init__(self):
@@ -176,13 +133,13 @@ if __name__ == '__main__':
             algorithm))
     client.tls_set(ca_certs=ca_certs, tls_version=ssl.PROTOCOL_TLSv1_2)
 
-    device = Device()
+    publisher = Publisher()
 
-    client.on_connect = device.on_connect
-    client.on_publish = device.on_publish
-    client.on_disconnect = device.on_disconnect
-    client.on_subscribe = device.on_subscribe
-    client.on_message = device.on_message
+    client.on_connect = publisher.on_connect
+    client.on_publish = publisher.on_publish
+    client.on_disconnect = publisher.on_disconnect
+    client.on_subscribe = publisher.on_subscribe
+    client.on_message = publisher.on_message
 
     client.connect(mqtt_bridge_hostname, mqtt_bridge_port)
 
@@ -196,7 +153,7 @@ if __name__ == '__main__':
     mqtt_config_topic = '/devices/{}/config'.format(device_id)
 
     # Wait up to 5 seconds for the device to connect.
-    device.wait_for_connection(5)
+    publisher.wait_for_connection(5)
 
     # Subscribe to the config topic.
     client.subscribe(mqtt_config_topic, qos=1)
@@ -205,12 +162,12 @@ if __name__ == '__main__':
     for _ in range(num_messages):
         # In an actual device, this would read the device's sensors. Here,
         # you update the temperature based on whether the fan is on.
-        device.update_sensor_data()
+        publisher.update_sensor_data()
 
         # Report the device's temperature to the server by serializing it
         # as a JSON string.
         payload = json.dumps(
-            {'temperature': device.temperature, 'device_id': device_id})
+            {'temperature': publisher.temperature, 'device_id': device_id})
         print('Publishing payload', payload)
         client.publish(mqtt_telemetry_topic, payload, qos=1)
         # Send events every second.
